5d1b3a0e07fe97af9ee67bad95c60635b44dc363
calonthar <calonthar@c3cff89a-8ba0-11dd-a9db-710176c1da70>
2012-08-23 20:20:53 +0000

    - File relocation
    git-svn-id: https://praisebasepresenter.googlecode.com/svn/trunk@146 c3cff89a-8ba0-11dd-a9db-710176c1da70

--- Presenter/Forms/LoadingScreen.Designer.cs
+++ Presenter/Forms/LoadingScreen.Designer.cs
@@ -62,7 +62,7 @@ namespace Pbp.Forms
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(299, 175);
+            this.label1.Location = new System.Drawing.Point(292, 172);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(12, 16);
             this.label1.TabIndex = 1;
