ac420801cb7c8be9616c84f0d714a48647406351
calonthar@gmail.com <calonthar@gmail.com@c3cff89a-8ba0-11dd-a9db-710176c1da70>
2012-09-03 18:34:02 +0000

    openlyrics integration pt1
    git-svn-id: https://praisebasepresenter.googlecode.com/svn/trunk@161 c3cff89a-8ba0-11dd-a9db-710176c1da70

---
+++ Presenter/IO/OpenLyricsSongFileReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Pbp.Properties;
+
+namespace Pbp
+{
+    class OpenLyricsSongFileReader : SongFileReader
+    {
+        public override Song load(string filename)
+        {
+            Song sng = new Song();
+
+            // Default font settings if values in xml invalid
+            sng.TextFont = Settings.Default.ProjectionMasterFont;
+            sng.TextColor = Settings.Default.ProjectionMasterFontColor;
+            sng.TranslationFont = Settings.Default.ProjectionMasterFontTranslation;
+            sng.TranslationColor = Settings.Default.ProjectionMasterTranslationColor;
+            sng.TextLineSpacing = Settings.Default.ProjectionMasterLineSpacing;
+
+            // Init xml
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filename);
+            XmlElement xmlRoot = xmlDoc.DocumentElement;
+
+            if (xmlRoot.Name != "song" || xmlRoot.GetAttribute("version") != "0.8")
+            {
+                throw new Exception("Invalid file type!");
+            }
+
+            if (xmlRoot["properties"] == null || xmlRoot["properties"] == null)
+            {
+                throw new Exception("Incomplete file!");
+            }
+
+            // Title
+            sng.Title = xmlRoot["properties"]["titles"]["title"].InnerText;
+
+            sng.updateSearchText();
+            return sng;
+        }
+    }
+}
