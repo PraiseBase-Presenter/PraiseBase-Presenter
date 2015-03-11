2fb2abd75c5715d3e15b1444c6c746806b2f7637
Nicolas Perrenoud <mail@nicu.ch>
2015-03-11 20:01:36 +0100

    Moved FileResources to configuration project

--- Configuration/Properties/FileResources.Designer.cs
+++ Configuration/Properties/FileResources.Designer.cs
@@ -22,7 +22,7 @@ namespace PraiseBase.Presenter.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class FileResources {
+    public class FileResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace PraiseBase.Presenter.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PraiseBase.Presenter.Properties.FileResources", typeof(FileResources).Assembly);
@@ -51,7 +51,7 @@ namespace PraiseBase.Presenter.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -76,7 +76,7 @@ namespace PraiseBase.Presenter.Properties {
         ///  The licenses for most software and other practical works are designed
         ///to [rest of string was truncated]&quot;;.
         /// </summary>
-        internal static string License {
+        public static string License {
             get {
                 return ResourceManager.GetString("License", resourceCulture);
             }
