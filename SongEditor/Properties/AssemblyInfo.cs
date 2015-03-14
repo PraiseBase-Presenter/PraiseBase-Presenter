8f047c229d3de04e77b09136cd511c3d055ce04e
Nicolas Perrenoud <mail@nicu.ch>
2015-03-14 16:03:38 +0100

    Update editor assembly info

---
+++ SongEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PraiseBase.Presenter")]
+[assembly: AssemblyDescription("Projects song lyrics and images using multiple screens")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PraiseBase.org")]
+[assembly: AssemblyProduct("PraiseBase Presenter")]
+[assembly: AssemblyCopyright("Copyright © PraiseBase.org 2008")]
+[assembly: AssemblyTrademark("PraiseBase")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("60e601e3-e10c-43fd-bf24-577d41ade125")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
