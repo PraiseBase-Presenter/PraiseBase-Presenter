b29825eed27149908a03ce069a8add348f69bfc2
Nicolas Perrenoud <nicu@lavine.ch>
2015-03-24 18:31:12 +0100

    Removed Abstract song editor child, was not used

--- Presenter/Editor/SongEditorChild.cs
+++ Presenter/Editor/SongEditorChild.cs
@@ -23,16 +23,19 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 using PraiseBase.Presenter.Controls;
 using PraiseBase.Presenter.Forms;
 using PraiseBase.Presenter.Manager;
 using PraiseBase.Presenter.Model.Song;
+using PraiseBase.Presenter.Projection;
 using PraiseBase.Presenter.Properties;
+using PraiseBase.Presenter.Template;
 
 namespace PraiseBase.Presenter.Editor
 {
-    public partial class SongEditorChild : AbstractSongEditorChild
+    public partial class SongEditorChild : Form
     {
         #region Public fields
 
@@ -43,8 +46,46 @@ namespace PraiseBase.Presenter.Editor
 
         #endregion
 
-        public SongEditorChild(Settings settings, ImageManager imgManager, Song sng) : base(settings, imgManager)
+        #region Internal variables
+
+        /// <summary>
+        /// Index of currently selected part
+        /// </summary>
+        protected int CurrentPartId;
+
+        /// <summary>
+        /// Index of currently selected slide
+        /// </summary>
+        protected int CurrentSlideId;
+
+        /// <summary>
+        /// Settings instance holder
+        /// </summary>
+        protected readonly Settings Settings;
+
+        /// <summary>
+        /// Song template mapper instance
+        /// </summary>
+        protected SongTemplateMapper TemplateMapper;
+
+        /// <summary>
+        /// Image manager instance
+        /// </summary>
+        protected readonly ImageManager ImgManager;
+
+        /// <summary>
+        /// Slide text formatting mapper
+        /// </summary>
+        protected readonly ISlideTextFormattingMapper<Song> PreviewFormattingMapper = new SongSlideTextFormattingMapper();
+
+        #endregion
+
+        public SongEditorChild(Settings settings, ImageManager imgManager, Song sng)
         {
+            Settings = settings;
+            ImgManager = imgManager;
+            TemplateMapper = new SongTemplateMapper(Settings);
+
             Song = sng;
 
             InitializeComponent();
@@ -80,6 +121,11 @@ namespace PraiseBase.Presenter.Editor
 
             // Preview
             PreviewSlide();
+        }
+
+        public void SetWindowTitle(string title)
+        {
+            Text = title;
         }
 
         private void PopulateQa()
@@ -540,6 +586,44 @@ namespace PraiseBase.Presenter.Editor
         private void PreviewSlide()
         {
             pictureBoxPreview.Image = PreviewSlide(Song, textBoxSongText.Text, textBoxSongTranslation.Text);
+        }
+
+        protected Image PreviewSlide(Song sng, string currentText, string currentTranslationText)
+        {
+            SongSlide slide = (SongSlide) sng.Parts[CurrentPartId].Slides[CurrentSlideId].Clone();
+            slide.Text = currentText;
+            slide.TranslationText = currentTranslationText;
+            SlideTextFormatting slideFormatting = new SlideTextFormatting();
+
+            PreviewFormattingMapper.Map(sng, ref slideFormatting);
+
+            // Disabled for performance
+            slideFormatting.OutlineEnabled = false;
+            slideFormatting.SmoothShadow = false;
+
+            slideFormatting.ScaleFontSize = Settings.ProjectionFontScaling;
+            slideFormatting.SmoothShadow = false;
+
+            TextLayer sl = new TextLayer(slideFormatting)
+            {
+                MainText = slide.Lines.ToArray(),
+                SubText = slide.Translation.ToArray()
+            };
+
+            ImageLayer il = new ImageLayer(Settings.ProjectionBackColor);
+
+            IBackground bg = sng.Parts[CurrentPartId].Slides[CurrentSlideId].Background;
+            il.Image = ImgManager.GetImage(bg);
+
+            var bmp = new Bitmap(1024, 768);
+            Graphics gr = Graphics.FromImage(bmp);
+            gr.CompositingQuality = CompositingQuality.HighSpeed;
+            gr.SmoothingMode = SmoothingMode.HighSpeed;
+
+            il.WriteOut(gr, null);
+            sl.WriteOut(gr, null);
+
+            return bmp;
         }
 
         private void neueFolieToolStripMenuItem_Click(object sender, EventArgs e)
