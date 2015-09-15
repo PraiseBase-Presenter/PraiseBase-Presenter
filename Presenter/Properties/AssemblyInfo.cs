7b77e5ecac67cffba7e255516f62dfc385effc12
Nicolas Perrenoud <nicu@lavine.ch>
2015-09-15 09:04:16 +0200

    Set version to 1.2.4

--- Presenter/Properties/AssemblyInfo.cs
+++ Presenter/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyVersion("1.2.4")]
+[assembly: AssemblyFileVersion("1.2.4")]
 [assembly: NeutralResourcesLanguageAttribute("de-CH")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Test")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log.config", Watch = true)]
