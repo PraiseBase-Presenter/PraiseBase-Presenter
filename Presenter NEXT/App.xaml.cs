2f3cb2aa058ba54e558eaa7fec86b3865aee0401
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2016-12-02 08:23:21 +0100

    Initial commit for WPF-based GUI

---
+++ Presenter NEXT/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using PraiseBase.Presenter.Properties;
+using System.Windows;
+
+namespace PraiseBase.Presenter
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        
+    }
+}
---
+++ Presenter NEXT/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PraiseBase.Presenter
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
